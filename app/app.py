6e7d13551fcbaad1eb894eff7797b7cf5bbabf75
bourydamien <bourydamien1@gmail.com>
2024-10-03 23:20:58 +0200

    app modif

--- app/app.py
+++ app/app.py
@@ -37,7 +37,7 @@ async def predict(file: UploadFile = File(...)):
         
         # Charger les objets de prétraitement
         try:
-            (label_encoders, one_hot_columns, scaler, transformed_columns, lambda_params, catboost_model) = load_preprocessing_objects(MODEL_FOLDER)
+            (label_encoders, one_hot_columns, scaler, lambda_params, catboost_model) = load_preprocessing_objects(MODEL_FOLDER)
         except Exception as e:
             raise HTTPException(status_code=500, detail=f"Erreur lors du chargement des objets de prétraitement : {str(e)}")
 
